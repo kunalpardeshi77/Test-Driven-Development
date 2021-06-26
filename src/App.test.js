23e0c8b15fc91870028e7e73573e59ebe508b890
Kunal Pardeshi <kunapard@amdocs.com>
2021-06-26 14:33:04 +0530

    Removing data-test attribute is production env

--- src/App.test.js
+++ src/App.test.js
@@ -3,20 +3,32 @@ import Enzyme, { shallow } from 'enzyme';
 import EnzymeAdapter from 'enzyme-adapter-react-16';
 import App from './App';
 
-Enzyme.configure({ adapter: new EnzymeAdapter() })
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+/**
+ *  Factory function to create a ShallowWrapper for the App component.
+ * @function setup
+ * @returns {ShallowWrapper}
+ */
+const setup = () => shallow(<App />);
 
 test('renders without error', () => {
-    const wrapper = shallow(<App />)
-    // console.log("wrapper", wrapper.debug());
+    const wrapper = setup();
+    const appComponent = wrapper.find("[data-test='component-app']");
+    expect(appComponent.length).toBe(1);
     expect(wrapper.exists()).toBeTruthy();
 });
 
 test('renders button', () => {
-
+    const wrapper = setup();
+    const button = wrapper.find("[data-test='increment-button']");
+    expect(button.length).toBe(1);
 });
 
 test('renders counter display', () => {
-
+    const wrapper = setup();
+    const counterDisplay = wrapper.find("[data-test='counter-display']");
+    expect(counterDisplay.length).toBe(1);
 });
 
 test('counter starts at 0', () => {
